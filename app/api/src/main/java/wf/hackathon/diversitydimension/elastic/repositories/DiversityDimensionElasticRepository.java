c0b7e5658041d772ad7d8da53f66486c55c5d38e
vinaygajjar11111 <vinay.gajjar@hotmail.com>
2022-05-16 02:51:23 -0400

    Removed Gradle project files

--- app/api/src/main/java/wf/hackathon/diversitydimension/elastic/repositories/DiversityDimensionElasticRepository.java
+++
@@ -1,8 +0,0 @@
-package wf.hackathon.diversitydimension.elastic.repositories;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import wf.hackathon.diversitydimension.elastic.domain.DiversityDimension;
-
-public interface DiversityDimensionElasticRepository extends ElasticsearchRepository<DiversityDimension, String> {
-
-}
